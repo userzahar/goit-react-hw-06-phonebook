55208f23fbd5afbf244cdebaa9c3f70f20987495
userzahar <110730909+userzahar@users.noreply.github.com>
2023-04-17 23:38:53 +0300

    add localstorage

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,13 +3,6 @@ import { ContactList } from "./ContactList/ContactList";
 import { Filter } from "./Filter/Filter";
 
 export function App() {
-
-  //  useEffect(() => {
-
-  //   localStorage.setItem('contacts', JSON.stringify(contacts))
-
-  // }, [contacts])
-  
   return <>
       <h1>Phonebook</h1>
       <Form />
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,4 +1,32 @@
 import { reducer } from './reducer';
 import { configureStore } from '@reduxjs/toolkit';
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
 
-export const store = configureStore({ reducer });
+import storage from 'redux-persist/lib/storage';
+const persistConfig = {
+  key: 'root',
+  version: 1,
+  storage,
+  blacklist: ['filter'],
+};
+
+const persistedReducer = persistReducer(persistConfig, reducer);
+export const store = configureStore({
+  reducer: persistedReducer,
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+});
+export const persistor = persistStore(store);
